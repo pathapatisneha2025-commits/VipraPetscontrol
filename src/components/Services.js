81ef06876d27f0ffda5fdfb2ff2ef1996afb821d
Sneha-datla <datla.sneha1@gmail.com>
2025-12-22 13:24:08 +0530

    changes made

--- src/components/Services.js
+++ src/components/Services.js
@@ -133,8 +133,8 @@ export default function Services() {
 }
 
 .icon-box img {
-  width: 80px;
-  height: 80px;
+  width: 100%;
+  height: 100;
   object-fit: cover;
   border-radius: 16px;
   background: linear-gradient(135deg, #143b73, #0d2545);
